b88032a378f8aa9085ca0856ddf765df14699210
Dy <92539681+Dy1337@users.noreply.github.com>
2022-06-02 15:00:46 +0100

    Create node.js

---
+++ node.js
@@ -0,0 +1,15 @@
+design_patterns_in_typescript\proxy> tsc --target ES5 .\demo.ts
+design_patterns_in_typescript\proxy> node .\demo.js
+`doAction` of Proxy( proxy1 )
+creating a new RealSubject.
+`doAction` of RealSubject proxy1 is being called!
+`doAction` of Proxy( proxy1 )
+`doAction` of RealSubject proxy1 is being called!
+`doAction` of Proxy( proxy2 )
+creating a new RealSubject.
+`doAction` of RealSubject proxy2 is being called!
+`doAction` of RealSubject proxy2 is being called!
+`doAction` of Proxy( proxy2 )
+`doAction` of RealSubject proxy2 is being called!
+`doAction` of Proxy( proxy1 )
+`doAction` of RealSubject proxy1 is being called!
